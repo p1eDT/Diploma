942878747bd6258f78287241001444703c75f052
p1eDT <error555@yandex.ru>
2023-07-17 03:20:45 +0500

    Add CheckBox and builder Add DeleteTestSuiteTests

---
+++ Core/Selenium/Elements/CheckBoxBuilder.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Selenium.Elements
+{
+    public class CheckBoxBuilder
+    {
+        public static CheckBox GetCheckBoxInTableByName(string name)
+        {
+            return new CheckBox(By.XPath($"//a[text()='{name}']/ancestor::tr//input[@type='checkbox']"));
+        }
+    }
+}
---
+++ Test/UiTests/DeleteTestSuiteTests.cs
@@ -0,0 +1,43 @@
+﻿using Bogus.DataSets;
+using BussinesObject.UI.Pages;
+using Core.Selenium;
+using Core.Selenium.Elements;
+using OpenQA.Selenium;
+
+namespace Test.UiTests
+{
+    public class DeleteTestSuiteTests:BaseTest
+    {
+        [Test]
+        public void Test()
+        {
+            new LoginPage()
+                .OpenPage()
+                .Login()
+                .SelectProject("Test1Project")
+                .OpenSuites();
+
+            var checkBox = CheckBoxBuilder.GetCheckBoxInTableByName("test3");
+
+            checkBox.SetCheckBoxState(true);
+
+            //удаление через dropdawn элемент, который появляется после выбора какого-нибудь элемента
+            //рядом с кнопкой фильтра вверху таблицы
+            Button selectedButton = new(By.XPath("//button[@type='button' and @class='button']"));
+            By deleteButton = By.XPath("//div[contains(text(),'Delete')]");
+            selectedButton.ClickElementViaJs();
+            Browser.Instance.Driver.FindElement(deleteButton).Click();
+
+            //модалка с подтвеждением удаления
+            CheckBox shureCheckBox = new(By.XPath("//section[@class='modal-card-body']/descendant::input"));
+            shureCheckBox.SetCheckBoxState(true);
+            Button deleteButtonSure = new("danger");
+            deleteButtonSure.ClickElementViaJs();
+
+            //Check delete testSuite;
+            //Assert.IsNull(selectedButton);
+            
+
+        }
+    }
+}
